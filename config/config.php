f95f07f5194da7b73d717a2421f1778e8b974d4a
Hannu Kulmala <hannu.kulmala@edu.sasky.fi>
2021-11-26 14:01:05 +0200

    tietokantakäsittelyn kääreluokka

--- config/config.php
+++ config/config.php
@@ -1,5 +1,11 @@
 <?php
 $config = array(
+    "db" => array(
+        "dbname" => $_SERVER["DB_DATABASE"],
+        "username" => $_SERVER["DB_USERNAME"],
+        "password" => $_SERVER["DB_PASSWORD"],
+        "host" => "localhost"
+    ),
     "urls" => array(
         "baseUrl" => "/~hkulmala/talkoot"
     )
