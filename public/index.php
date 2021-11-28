905925f1f7b0208223dcdce3b8cfa3a69225dc1a
Hannu Kulmala <hannu.kulmala@edu.sasky.fi>
2021-11-28 12:12:28 +0200

    lomaketiedon tarkistaminen

--- public/index.php
+++ public/index.php
@@ -28,18 +28,21 @@ switch ($request) {
     		echo $templates->render('tapahtumanotfound');
     	}
     	break;
-	case '/lisaa_tili':
-		if (isset($_POST['laheta'])) {
-			$formdata = cleanArrayData($_POST);
-			require_once MODEL_DIR . 'henkilo.php';
-			$salasana = password_hash($formdata['salasana1'], PASSWORD_DEFAULT);
-			$id = lisaaHenkilo($formdata['nimi'],$formdata['email'],$salasana);
-			echo "Tili on luotu tunnisteella $id";
-			break;
-		} else {
-			echo $templates->render('lisaa_tili');
-			break;
-		}    
+		 case '/lisaa_tili':
+			if (isset($_POST['laheta'])) {
+			  $formdata = cleanArrayData($_POST);
+			  require_once CONTROLLER_DIR . 'tili.php';
+			  $tulos = lisaaTili($formdata);
+			  if ($tulos['status'] == "200") {
+				 echo "Tili on luotu tunnisteella $tulos[id]";
+				 break;
+			  }
+			  echo $templates->render('lisaa_tili', ['formdata' => $formdata, 'error' => $tulos['error']]);
+			  break;
+			} else {
+			  echo $templates->render('lisaa_tili', ['formdata' => [], 'error' => []]);
+			  break;
+			}
     default:
     	echo $templates->render('notfound');
     }
--- src/view/lisaa_tili.php
+++ src/view/lisaa_tili.php
@@ -1,23 +1,26 @@
 <?php $this->layout('template', ['title' => 'Uuden tilin luonti']) ?>
 <h1>Uuden tilin luonti</h1>
 <form action="" method="POST">
-<div>
-<label for="nimi">Nimi:</label>
-<input id="nimi" type="text" name="nimi">
-</div>
-<div>
-<label for="email">Sähköposti:</label>
-<input id="email" type="email" name="email">
-</div>
-<div>
-<label for="salasana1">Salasana:</label>
-<input id="salasana1" type="password" name="salasana1">
-</div>
-<div>
-<label for="salasana2">Salasana uudelleen:</label>
-<input id="salasana2" type="password" name="salasana2">
-</div>
-<div>
-<input type="submit" name="laheta" value="Luo tili">
-</div>
+  <div>
+    <label for="nimi">Nimi:</label>
+    <input id="nimi" type="text" name="nimi" value="<?= getValue($formdata,'nimi') ?>">
+    <div class="error"><span><?= getValue($error,'nimi'); ?></span></div>
+  </div>
+  <div>
+    <label>Sähköposti:</label>
+    <input type="text" name="email" value="<?= getValue($formdata,'email') ?>">
+    <div class="error"><?= getValue($error,'email'); ?></div>
+  </div>
+  <div>
+    <label>Salasana:</label>
+    <input type="password" name="salasana1">
+    <div class="error"><?= getValue($error,'salasana'); ?></div>
+  </div>
+  <div>
+    <label>Salasana uudelleen:</label>
+    <input type="password" name="salasana2">
+  </div>
+  <div>
+    <input type="submit" name="laheta" value="Luo tili">
+  </div>
 </form>
