e609abfcde0c3195756e9a44bfd5336d45994842
Hannu Kulmala <hannu.kulmala@edu.sasky.fi>
2021-11-30 11:39:12 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -6,9 +6,20 @@
         <link href="styles/styles.css" rel="stylesheet">
     </head>
     <body>
-        <header>
-            <h1><a href="<?=BASEURL?>">Frisbeegolfseura</a></h1>
-        </header>
+    <header>
+      <h1><a href="<?=BASEURL?>">lanify</a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
+    </header>
+
         <section>
             <?=$this->section('content')?>
         </section>
