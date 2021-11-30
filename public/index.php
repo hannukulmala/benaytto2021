db9b70a218a72f12d9fa0584e75a190afea77b78
Hannu Kulmala <hannu.kulmala@edu.sasky.fi>
2021-11-30 11:41:25 +0200

    uloskirjautuminen

--- public/index.php
+++ public/index.php
@@ -57,6 +57,11 @@ switch ($request) {
 				  echo $templates->render('kirjaudu', [ 'error' => []]);
 				}
 				break;
+			case "/logout":
+				require_once CONTROLLER_DIR . 'kirjaudu.php';
+				logout();
+				header("Location: " . $config['urls']['baseUrl']);
+				break;
     default:
     	echo $templates->render('notfound');
     }
